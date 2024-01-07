62900ca85f8e62c1bca4b09c31001d3fca45899e
Liam <43306264+LiamNgo24@users.noreply.github.com>
2024-01-07 10:08:11 +0700

    FIRST COMMIT PRAC 1

---
+++ 1.student.mark.py
@@ -0,0 +1,14 @@
+def show_menu():
+    print("\n--- Student Mark Management System ---")
+    print("1. Input number of students in a class")
+    print("2. Input student information")
+    print("3. Input number of courses")
+    print("4. Input course information")
+    print("5. Select a course and input marks for students")
+    print("6. List courses")
+    print("7. List students")
+    print("8. Show student marks for a given course")
+    print("9. Exit")
+    choice = input("Enter your choice: ")
+    return choice
+
